e128a473807d64fde55056343c61e1ad359c72f7
pironmind <paulbolhaskih@yahoo.com>
2022-08-16 17:28:29 +0300

    add Farming/Staking contract

---
+++ contracts/interfaces/IFungibleToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0 <1.0.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IFungibleToken is IERC20 {
+    function mint(address to, uint256 amount) external;
+    function burn(uint256 amount) external;
+}
