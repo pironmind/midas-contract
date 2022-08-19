447237f79120c1bd666483d850345b9df61942ae
pironmind <paulbolhaskih@yahoo.com>
2022-08-19 12:29:47 +0300

    add burn for burner operator, update comments, change token destination to dead address

--- contracts/light_bridge/Locker.sol
+++ contracts/light_bridge/Locker.sol
@@ -4,36 +4,62 @@ pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "../RBAC.sol";
 
 /*
  * @title Locker - used for lock tokens on main chain.
  *
  */
-contract Locker {
+contract Locker is RBAC {
 	using SafeERC20 for IERC20;
 
-	IERC20 public midas;
+	// @dev MIDAS token address.
+	IERC20 public immutable midas;
 
+	// @dev Event triggered when balance locked.
 	event Locked(address from, address to, uint256 amount);
 
-	constructor(IERC20 _midasToken) {
+	constructor(IERC20 _midasToken, address _admin)
+		RBAC(_admin)
+	{
 		require(address(_midasToken) != address(0), "Locker: constructor zero token");
 
 		midas = _midasToken;
 	}
 
+	/**
+     * @dev Lock tokens on second chain by sender account.
+     */
 	function lock() external {
 		uint256 accountBalance = midas.balanceOf(msg.sender);
-		midas.safeTransferFrom(msg.sender, address(this), accountBalance);
+		midas.safeTransferFrom(msg.sender, address(0xdead), accountBalance);
 		emit Locked(msg.sender, msg.sender, accountBalance);
 	}
 
-	// @dev Use this function for CA.
+	/**
+     * @dev Lock on second chain with change recipient address.
+     */
 	function lockFor(address _account) external {
-		require(_account != address(0), "Locker: zero address");
+		require(
+			_account != address(0) &&
+			_account != address(0xdead),
+			"Locker: zero address"
+		);
 
 		uint256 accountBalance = midas.balanceOf(msg.sender);
-		midas.safeTransferFrom(msg.sender, address(this), accountBalance);
+
 		emit Locked(msg.sender, _account, accountBalance);
+	}
+
+	/**
+     * @dev Burn missing tokens from this CA.
+     */
+	function burn(address[] calldata _accounts, uint256[] calldata _balances) external onlyRole(BURNER_ROLE) {
+		require(_accounts.length == _balances.length, "Invalid input length");
+
+		for (uint256 i; i < _accounts.length; i++) {
+			midas.safeTransfer(address(0xdead), _balances[i]);
+			emit Locked(address(this), _accounts[i], _balances[i]);
+		}
 	}
 }
