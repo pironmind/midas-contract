178480cf5c96d29798100383ab6b42c991c9a917
pironmind <paulbolhaskih@yahoo.com>
2022-09-01 12:21:47 +0300

    add max total supply

--- contracts/MidasToken.sol
+++ contracts/MidasToken.sol
@@ -5,7 +5,12 @@ pragma solidity ^0.8.15;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./RBAC.sol";
 
+/**
+ *
+ */
 contract MidasToken is ERC20, RBAC {
+	uint256 public constant MAX_TOTAL_SUPPLY = 5_000_000e18; // 5,000,000
+
 	constructor(address _admin)
 		ERC20("Midas", "MIDAS")
 		RBAC(_admin)
@@ -15,6 +20,7 @@ contract MidasToken is ERC20, RBAC {
      * @dev Implement {ERC20._mint} functionality with MINTER_ROLE permission.
 	 */
 	function mint(address _to, uint256 _amount) external onlyRole(MINTER_ROLE) {
+		require(totalSupply() + _amount <= MAX_TOTAL_SUPPLY, "MidasToken: MAX_SUPPLY_OVERFLOW");
 		_mint(_to, _amount);
 	}
 
