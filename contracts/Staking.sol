e432372d8bb3136a81c5b74891bc1dcfeae34f6d
pironmind <paulbolhaskih@yahoo.com>
2022-10-02 11:58:52 +0300

    post audit fix "Redundant import"

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -5,9 +5,7 @@ pragma solidity =0.8.15;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/Multicall.sol";
-import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "./interfaces/IFungibleToken.sol";
 
 // @title Staking - staking contract simplified MasterChef.
